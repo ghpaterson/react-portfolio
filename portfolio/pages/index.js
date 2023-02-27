dd82c97bc7935ba521c974348ad5a4b0c906454a
Graeme Paterson <ghpaterson@gmail.com>
2023-02-27 11:20:42 +0000

    iniital navBar setup

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -11,7 +11,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">Developed by Graeme</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              </li>
+              <li>
+                <a className="bg-cyan-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Github</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </>
   );
 }
