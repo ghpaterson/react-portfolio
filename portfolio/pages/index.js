cf32c38ef75746205e186081d3ff0e3a46da544f
Graeme Paterson <ghpaterson@gmail.com>
2023-02-27 12:02:54 +0000

    adding paragraphs and template for where the projects will be shown

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -14,28 +14,43 @@ export default function Home() {
       <main className="bg-gray-50">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">Developed by Graeme</h1>
+            <h1 className="text-xl ml-5">Developed by Graeme</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
               </li>
               <li>
-                <a className="bg-cyan-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Github</a>
+                <a className="bg-yellow-500 text-white px-4 py-2 border-none rounded-md ml-8 mr-5" href="#">Github</a>
               </li>
             </ul>
           </nav>
           <div className="text-center p-10">
             <h2 className="text-5xl py-2 text-yellow-500 font-medium">Graeme Paterson</h2>
-            <h3 className="text-2xl py-2">Software Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
-              Junior Software developer focusing on Frontend 
+            <h3 className="text-2xl py-2 text-gray-700">Software Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-700">
+              Junior Software developer focusing on Frontend. Interested UI/UX
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <AiFillLinkedin />
             <AiFillGithub />
           </div>
-          <div></div>
+          {/* <div>Image will go here</div>  */}
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Projects</h3>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              Here are screen shots of some of the projects I am proud of...
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              Here is some more text that I ma going to waffle on about myself if I need to 
+              because I have to get used to talking about myself.
+            </p>
+          </div>
+
+          
         </section>
       </main>
     </>
