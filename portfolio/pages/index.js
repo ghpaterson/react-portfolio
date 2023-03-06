5741a8dc44e7fd419815adb29af4cd91abb431ad
Graeme Paterson <ghpaterson@gmail.com>
2023-03-06 18:01:21 +0000

    footer and projects put into own element

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Footer from "./footer";
+import Projects from "./projects";
 import { BsFillMoonFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import {
@@ -14,21 +16,13 @@ import {
 import { DiRuby } from "react-icons/di";
 import Image from "next/image";
 import avatar from "../public/avatar.png";
-import Ecoliday from "../public/Ecoliday.png";
-import mineshaft from "../public/mineshaft.png";
-import weather from "../public/weather.png";
-import homepage from "../public/homepage.png";
 import { useState } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
-  const githubCVUrl = "https://github.com/ghpaterson/CV";
   const gitHubUrl = "https://github.com/ghpaterson";
   const linkedInUrl = "https://www.linkedin.com/in/graeme-paterson-25b096268/";
-  const ecolidayUrl = "https://github.com/ghpaterson/Ecoliday";
-  const mineshaftUrl = "https://github.com/ghpaterson/acebook-mineshaft";
-  const weatherUrl = "https://github.com/ghpaterson/weather-app";
-  const recipeaseURL = "https://github.com/ghpaterson/recipease";
+  const githubCVUrl = "https://github.com/ghpaterson/CV";
 
   return (
     <>
@@ -41,13 +35,13 @@ export default function Home() {
         </Head>
         <main className="bg-gray-100 px-10 md:px-20 lg:px-30 dark:bg-gray-800">
           <section className="min-h-screen">
-            <nav className="py-10 mb-2 flex justify-between">
+            <nav className=" static py-10 mb-2 flex justify-between">
               <h1 className="text-xl ml-5 dark:text-gray-100"></h1>
               <ul className="flex items-center">
                 <li>
                   <BsFillMoonFill
                     onClick={() => setDarkMode(!darkMode)}
-                    className="cursor-pointer text-2xl hover:animate-pulse text-gray-700 dark:text-yellow-500"
+                    className="cursor-pointer text-2xl hover:animate-pulse text-gray-700 dark:text-yellow-500 "
                   />
                 </li>
                 <li>
@@ -74,10 +68,20 @@ export default function Home() {
               </p>
             </div>
             <div className="text-3xl flex justify-center gap-16 py-2 text-gray-600 cursor-pointer dark:text-gray-100">
-              <a href={linkedInUrl} target="_blank" rel="noreferrer">
+              <a
+                className=" dark:hover:text-yellow-500"
+                href={linkedInUrl}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <AiFillLinkedin />
               </a>
-              <a href={gitHubUrl} target="_blank" rel="noreferrer">
+              <a
+                className=" dark:hover:text-yellow-500"
+                href={gitHubUrl}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <AiFillGithub />
               </a>
             </div>
@@ -96,81 +100,8 @@ export default function Home() {
               <SiCss3 />
             </div>
           </section>
-
-          <section>
-            <div className="flex justify-center pt-6 mt-10">
-              <h3 className="text-5xl py-1 text-gray-700 dark:text-gray-100 dark:text-yellow-500">
-                Projects
-              </h3>
-            </div>
-
-            <div className="flex justify-center my-6">
-              <div>
-                <a href={ecolidayUrl} target="_blank" rel="noreferrer">
-                  <h3 className="text-2xl hover:text-yellow-500 pb-2 text-gray-600 dark:text-gray-100 dark:hover:text-yellow-500">
-                    {" "}
-                    Ecoliday{" "}
-                  </h3>
-                </a>
-                <p className="text-l pb-2 text-gray-600 dark:text-gray-100">
-                  Users can estimate their carbon emissions based of travel
-                  based on different modes of transport.
-                </p>
-                <Image src={Ecoliday} width={800} className="rounded-lg" />
-              </div>
-            </div>
-            <div className="flex justify-center my-10 mb-6">
-              <div>
-                <a href={recipeaseURL} target="_blank" rel="noreferrer">
-                  <h3 className="text-xl pb-2 hover:text-yellow-500 text-gray-600 dark:text-gray-100 dark:hover:text-yellow-500">
-                    {" "}
-                    Recipeas{" "}
-                  </h3>
-                </a>
-                <p className="text-l pb-2 text-gray-600 dark:text-gray-100">
-                  Food recipe library allowing users to browse by category or
-                  search for ingredients using the Spoonacular API
-                </p>
-                <Image src={homepage} width={800} className="rounded-lg" />
-              </div>
-            </div>
-            <div className="flex justify-center my-10">
-              <div>
-                <a href={mineshaftUrl} target="_blank" rel="noreferrer">
-                  <h3 className="text-xl pb-2 hover:text-yellow-500 text-gray-600 dark:text-gray-100 dark:hover:text-yellow-500">
-                    {" "}
-                    Mineshaft{" "}
-                  </h3>
-                </a>
-                <p className="text-l pb-2 text-gray-600 dark:text-gray-100">
-                  Facebook clone using the MERN stack
-                </p>
-                <Image src={mineshaft} width={800} className="rounded-lg" />
-              </div>
-            </div>
-            <div className="flex justify-center my-10 mb-6">
-              <div>
-                <a href={weatherUrl} target="_blank" rel="noreferrer">
-                  <h3 className="text-xl pb-2 hover:text-yellow-500 text-gray-600 dark:text-gray-100 dark:hover:text-yellow-500">
-                    {" "}
-                    Weather App{" "}
-                  </h3>
-                </a>
-                <p className="text-l pb-2 text-gray-600 dark:text-gray-100">
-                  Weather based on your current location using the Open-Meteo
-                  API
-                </p>
-                <Image src={weather} width={800} className="rounded-lg" />
-              </div>
-            </div>
-          </section>
-          <section>
-            <div>
-              <h1 className="text-l ml-5 pb-10 dark:text-gray-100">
-                Developed by Graeme Paterson
-              </h1>
-            </div>
-          </section>
+          <Projects />
+          <Footer />
         </main>
       </div>
     </>
