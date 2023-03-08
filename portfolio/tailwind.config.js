c3ca5bcfaec23e0a04c06cb838a09dfa21c7af9c
Graeme Paterson <ghpaterson@gmail.com>
2023-03-08 17:49:44 +0000

    changes to flex positioning and colour changes

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        bittersweet: "#E87461",
+        seagreen: "#519872",
+        lazuli: "#33658A",
+        moonstone: "#7FB7BE",
+        eggplant: "#714955",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
