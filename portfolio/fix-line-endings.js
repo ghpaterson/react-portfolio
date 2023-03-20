9e57998cdb5830f4ae935d2e5a654edef7295f03
Graeme Paterson <ghpaterson@gmail.com>
2023-03-20 13:03:45 +0000

    fix-line-endings deleted

--- portfolio/fix-line-endings.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const filePath = "node_modules/color-name/LICENSE";
-let fileContent = fs.readFileSync(filePath, "utf8");
-
-fileContent = fileContent.replace(/\r\n/g, "\n");
-
-fs.writeFileSync(filePath, fileContent);
